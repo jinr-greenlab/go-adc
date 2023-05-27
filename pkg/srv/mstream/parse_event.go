43b5540713dd6830bbcf054440589240197d492c
SPAleksey <alekseysp@gmail.com>
2023-05-27 09:03:42 +0400

    fix inspected issues

--- pkg/srv/mstream/parse_event.go
+++ pkg/srv/mstream/parse_event.go
@@ -3,6 +3,7 @@ package mstream
 import (
 	"encoding/binary"
 	"encoding/json"
+	"sort"
 
 	"jinr.ru/greenlab/go-adc/pkg/log"
 )
@@ -196,6 +197,9 @@ func BuildStructToApi(Ev MstreamHeader, EventNumber uint32) RepeatToApi {
 		RepeatToApi.ChannelVoltage = append(RepeatToApi.ChannelVoltage, ChannelVoltage)
 	}
 
+	sort.Slice(RepeatToApi.ChannelVoltage, func(i, j int) bool {
+		return RepeatToApi.ChannelVoltage[i].Channel < RepeatToApi.ChannelVoltage[j].Channel
+	})
 	return RepeatToApi
 }
 
